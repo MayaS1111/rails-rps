8a0daed9c2a83f579e7c59cebaae616dc309847b
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-04 18:08:24 +0000

    Controller and routes created

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rails-rps
 
 Target: https://rps.matchthetarget.com/
+
+Run: bin/dev
---
+++ app/controllers/zerba_controller.rb
@@ -0,0 +1,17 @@
+class ZebraController < ApplicationController
+  
+  def rules 
+    render({:template => "game_templates/rules"})
+  end
+  def rock 
+    render({:template => "game_templates/play_rock"})
+  end
+
+  def paper
+    render({:template => "game_templates/play_paper"})
+  end
+
+  def scissors 
+    render({:template => "game_templates/play_scissors"})
+  end
+end  
